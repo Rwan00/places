eeadc88cbecaac7bc0af6258aff14b2c2d1b0004
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2024-02-01 03:00:11 +0200

    add user_places provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:places/screens/places.dart';
 
 import 'theme/body_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
