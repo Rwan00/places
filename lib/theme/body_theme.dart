d95ee54f6a0a77f5b9162fcbe5c07e9e06e31503
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2024-02-01 02:12:09 +0200

    add theme & files

---
+++ lib/theme/body_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final ColorScheme colorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromRGBO(49, 48, 77,1),
+  background: const Color.fromRGBO(22, 26, 48,1),
+  brightness: Brightness.dark
+);
