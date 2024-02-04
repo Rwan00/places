cacf1bb0cae91b8185bf92dba817e25c4668c2d1
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2024-02-04 03:50:34 +0200

    get the path of image

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,14 +1,19 @@
 import 'dart:io';
-
+import 'dart:developer';
+import 'package:path_provider/path_provider.dart' as sysPaths;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:path/path.dart' as path;
 import '../models/place.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title,File image,PlaceLocation location) {
-    final newPlace = Place(title: title,image: image, location: location);
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await sysPaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(image.path);
+    final copiedImg = await image.copy("${appDir.path}/$fileName");
+    log(copiedImg.path);
+    final newPlace = Place(title: title, image: image, location: location);
     state = [newPlace, ...state];
   }
 }
